a828c93c1d33d77effd7fc074fffd61117a00306
ichirukyn <ichiru.kyn@bk.ru>
2024-02-22 20:08:46 +0300

    -- fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,14 +45,14 @@ const entity = [
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.env.${ process.env.NODE_ENV }`,
+      envFilePath: `.env`,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      password: String(process.env.DB_PASS),
       database: process.env.DB_NAME,
       entities: entity,
       logging: true,
